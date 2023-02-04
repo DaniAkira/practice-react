58445be9be546614e4a8624f2e3293f06a2be993
Daniel Akira <daakira@hotmail.com>
2023-02-04 17:56:26 -0300

    Adicionando responsividade na HomePage e nos Cards

--- my-csgo-grenades/src/components/Routes/script.js
+++ my-csgo-grenades/src/components/Routes/script.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from '../pages/HomePage/script'
+import { MiragePage } from '../pages/MiragePage/script';
 
 export const Rotas = () => (
     <BrowserRouter>
         <Routes>
             <Route exact path="/" element={<HomePage/>}></Route>
+            <Route path="/Mirage" element={<MiragePage/>}></Route>
         </Routes>
     </BrowserRouter>
 ) 
